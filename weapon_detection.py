1b1ea5208750e360dfbfbc866dd837cc2254824e
Vivek Surana <101589281+AnonymousLearnerVivek@users.noreply.github.com>
2025-03-24 19:27:47 +0530

    Add files via upload

---
+++ weapon_detection.py
@@ -0,0 +1,54 @@
+import numpy as np    # used for working with mathematical operations
+import cv2
+import imutils
+import DateTime
+
+
+# print(np.__version__)
+# print(cv2.__version__)
+# print(imutils.__version__)
+gun_cascade = cv2.CascadeClassifier('cascade.xml')
+camera = cv2.VideoCapture(0)
+
+firstFrame = None
+gun_exist = None
+
+while True:
+    ret, frame =  camera.read()
+    frame = imutils.resize(frame, width = 500)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    gun = gun_cascade.detectMultiScale(gray,
+                                       1.3, 5,
+                                       minSize=(100, 100))
+
+    if len(gun) > 0:
+        gun_exist = True
+
+    for (x, y, w, h) in gun:
+        frame = cv2.rectangle(frame,
+                              (x, y),
+                              (x+w, y+h),
+                              (255,0,0), 2)
+
+        roi_gray = gray[y : y + h, x : x+w]
+        roi_color = frame[y: y + h, x: x + w]
+
+    if firstFrame is None:
+        firstFrame = gray
+        continue
+
+    cv2.imshow("Security feed", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord('q'):
+        break
+
+if gun_exist:
+    print("Guns Detected")
+
+else:
+    print("Guns didn't detected")
+
+camera.release()
+cv2.destroyAllWindows()
